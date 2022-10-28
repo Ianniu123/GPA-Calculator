d7ed9b66b525a9a94384c858d9732371b3173ec0
Zhiyao Yu <985683179yzy>
2022-10-28 13:18:59 -0400

    added Object course_information

--- Source Code/View_Courses.java
+++ Source Code/View_Courses.java
@@ -5,15 +5,25 @@ public class View_Courses {
     JFrame frame;
     JPanel myPanel;
     JLabel label;
+    JScrollPane jsp;
 
-    public View_Courses(){
+    public View_Courses(int num){
         frame = new JFrame("View Courses");
         frame.setPreferredSize(new Dimension(400, 400));
         frame.setLocation(200,200);
 
         myPanel = new JPanel();
+        myPanel.setLayout(new GridLayout(25,40));
+        
+        for (int i = 1; i < 1001; i++){
+            JTextField text = new JTextField();
+            text.setEditable(false);
+            myPanel.add(text);
+        }
 
-        frame.add(myPanel);
+        jsp = new JScrollPane(myPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS) ;
+
+        frame.add(jsp);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -21,6 +31,6 @@ public class View_Courses {
     }
 
     public static void main(String[] args){
-        new View_Courses();
+        new View_Courses(6);
     }
 }
